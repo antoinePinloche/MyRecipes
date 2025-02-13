84bace160fbab2258e03c281481fe769c6a5ee2d
Antoine PINLOCHE <antoine.pinloche@gmail.com>
2025-02-13 13:43:16 +0100

    FoodTypeModelToCommand

--- MyRecepies/Mapper/FoodType/FoodTypeModelToCommand.cs
+++ MyRecepies/Mapper/FoodType/FoodTypeModelToCommand.cs
@@ -1,6 +1,18 @@
-﻿namespace MyRecipes.Web.API.Mapper.FoodType
+﻿using MyRecipes.Recipes.Application.FoodType.Command.CreateFoodType;
+using MyRecipes.Recipes.Application.FoodType.Command.UpdateFoodTypeById;
+using MyRecipes.Web.API.Models.Class.FoodType;
+
+namespace MyRecipes.Web.API.Mapper.FoodType
 {
-    public class FoodTypeModelToCommand
+    public static class FoodTypeModelToCommand
     {
+        public static CreateFoodTypeCommand ToCreateFoodTypeCommand(this CreateFoodTypeModel model)
+        {
+            return new CreateFoodTypeCommand(model.Name);
+        }
+        public static UpdateFoodTypeByIdCommand ToUpdateFoodTypeByIdCommand(this UpdateFoodTypeModel model, Guid id)
+        {
+            return new UpdateFoodTypeByIdCommand(id, model.Name);
+        }
     }
 }
